197542f43f6385b9ffb783ad021766d57630b3ce
socratesomiliadis <s.omiliadis@gmail.com>
2023-03-01 13:35:51 +0200

    Revamped course schema

--- lib/schemas/schemaTypes.ts
+++ lib/schemas/schemaTypes.ts
@@ -34,18 +34,18 @@ export const course = defineType({
       title: 'Course Code',
       validation: (rule) => rule.required().min(2).regex(/^\S*$/)
     }),
-    defineField({
-      name: 'courseTier',
-      type: 'number',
-      title: 'Course Tier',
-      options: {
-        list: [
-          { title: 'Tier 1', value: 1 },
-          { title: 'Tier 2', value: 2 },
-          { title: 'Tier 3', value: 3 }
-        ]
-      }
-    }),
+    // defineField({
+    //   name: 'courseTier',
+    //   type: 'number',
+    //   title: 'Course Tier',
+    //   options: {
+    //     list: [
+    //       { title: 'Tier 1', value: 1 },
+    //       { title: 'Tier 2', value: 2 },
+    //       { title: 'Tier 3', value: 3 }
+    //     ]
+    //   }
+    // }),
     defineField({
       name: 'courseDesc',
       type: 'text',
@@ -58,9 +58,75 @@ export const course = defineType({
       validation: (rule) => rule.regex(/^\#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/)
     }),
     defineField({
-      name: 'sections',
+      name: 'studentSections',
       type: 'array',
-      title: 'Sections',
+      title: 'Tier 1 Sections',
+      of: [
+        {
+          title: 'Section',
+          name: 'section',
+          type: 'object',
+          fields: [
+            { name: 'title', type: 'string', title: 'Title' },
+            {
+              name: 'text',
+              type: 'array',
+              title: 'Text',
+              of: [
+                {
+                  type: 'block'
+                }
+              ]
+            },
+            {
+              name: 'video',
+              type: 'file',
+              title: 'Video',
+              options: {
+                accept: 'video/*'
+              }
+            }
+          ]
+        }
+      ]
+    }),
+    defineField({
+      name: 'scholarSections',
+      type: 'array',
+      title: 'Tier 2 Sections',
+      of: [
+        {
+          title: 'Section',
+          name: 'section',
+          type: 'object',
+          fields: [
+            { name: 'title', type: 'string', title: 'Title' },
+            {
+              name: 'text',
+              type: 'array',
+              title: 'Text',
+              of: [
+                {
+                  type: 'block'
+                }
+              ]
+            },
+            {
+              name: 'video',
+              type: 'file',
+              title: 'Video',
+              options: {
+                accept: 'video/*'
+              }
+            }
+          ]
+        }
+      ]
+    }),
+    defineField({
+      name: 'expertSections',
+      type: 'array',
+      title: 'Tier 3 Sections',
       of: [
         {
           title: 'Section',
