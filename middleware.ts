126c8c409d6869ca5a954be7665dc09ff00c4f8e
socratesomiliadis <s.omiliadis@gmail.com>
2023-08-22 18:01:28 +0300

    ignored some api routes from middleware

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/onboarding"],
+  ignoredRoutes: ["/api/clerk-webhook", "/api/stripe-webhook"],
 });
 
 export const config = {
