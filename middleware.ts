f4976c8fe15435bf89c956f897fc898c190f88eb
Socrates Omiliadis <68393350+socratesomiliadis@users.noreply.github.com>
2023-08-25 20:03:23 +0300

    Made /studio public

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/onboarding"],
+  publicRoutes: ["/", "/sign-in", "/onboarding", "/studio"],
   ignoredRoutes: ["/api/clerk-webhook", "/api/stripe-webhook"],
 });
 
